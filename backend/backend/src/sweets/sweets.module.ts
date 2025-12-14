962290e6cecd0b2bd452ad2cfa48685e7953f00c
Sumit Singh <sumitsinghace2003@gmail.com>
2025-12-14 19:00:37 +0530

    installed shadcn/ui dependencies

--- backend/backend/src/sweets/sweets.module.ts
+++ backend/backend/src/sweets/sweets.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SweetsService } from './sweets.service';
 import { SweetsController } from './sweets.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [SweetsService],
-  controllers: [SweetsController]
+  controllers: [SweetsController],
+  providers: [SweetsService, PrismaService],
 })
 export class SweetsModule {}
